6be7ce4d22993f27ce1a9f35263111c445d038b5
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2022-10-28 19:49:42 +0300

    Добавьте файлы проекта.

---
+++ WinFormsLesson3/Form1.cs
@@ -0,0 +1,20 @@
+namespace WinFormsLesson3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2("Форма без переключения");
+            f.ShowDialog();
+            f.Close();
+            Form2 f2 = new Form2("Форма с переключением");
+            f2.Show();
+            f2.Close();
+        }
+    }
+}
---
+++ WinFormsLesson3/Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsLesson3
+{
+    public partial class Form2 : Form
+    {
+        public Form2(string s)
+        {
+            InitializeComponent();
+            this.Text = s;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
